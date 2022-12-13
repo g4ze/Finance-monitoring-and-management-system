d0260603607fa9e5108c71267e502492e352ac1b
g4ze <102874321+g4ze@users.noreply.github.com>
2022-12-13 10:15:59 +0530

    Update fetcher.py
    added comments

--- fetcher.py
+++ fetcher.py
@@ -1,11 +1,13 @@
 import gspread
+#importing gspread api
 class Reader:
     def reads(self):
         gc=gspread.service_account(filename='creds.json')
-        sh=gc.open('test')
-        ws=sh.sheet1
-        res=ws.get_all_records()
-        return res
+        #specifying credidentials file
+        sh=gc.open('test')  #telling the spreadsheet name
+        ws=sh.sheet1    #telling spreadsheet' workseet
+        res=ws.get_all_records()    #calling all records
+        return res  #returning the whole ws's info in form of a list of dictionaries
     
 #
 #
